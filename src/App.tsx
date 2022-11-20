9c3fdb8e7d7bdca0cc171a67605012214007f199
Araz0 <alhamdani.araz@gmail.com>
2022-11-20 16:37:04 +0100

    chore: moved home to pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 
-import { Home } from './components'
+import { Home } from './Pages'
 
 function App() {
   return (
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,19 @@
-import React, { memo, useCallback, useRef } from 'react'
+import React, { memo, useCallback, useRef, useState } from 'react'
 
 import { useParams } from 'react-router-dom'
 
-import { VideoPlayer } from '../VideoPlayer'
+import { VideoPlayer } from '../../components/VideoPlayer'
 
+const video1 = '/media/102.mp4'
+const video2 = '/media/103.mp4'
+
+// const url1 = 'https://media.w3.org/2010/05/bunny/movie.mp4'
+// const url2 = 'https://media.w3.org/2010/05/sintel/trailer.mp4'
 export const HomeRaw = () => {
   const { screenId } = useParams()
   const useVideoRef = useRef<any>()
   const useVideoRef2 = useRef<any>()
+  const [flag, setFlag] = useState(true)
 
   const handleClick = useCallback(() => {
     // eslint-disable-next-line no-console
@@ -25,19 +31,18 @@ export const HomeRaw = () => {
     }
   }, [])
   const handleSwitchSource = useCallback(() => {
-    if (
-      useVideoRef.current.src === 'https://media.w3.org/2010/05/bunny/movie.mp4'
-    ) {
-      useVideoRef.current.src = 'http://media.w3.org/2010/05/sintel/trailer.mp4'
-      useVideoRef2.current.src =
-        'http://media.w3.org/2010/05/sintel/trailer.mp4'
+    if (flag) {
+      useVideoRef.current.src = video2
+      useVideoRef2.current.src = video2
+      setFlag(false)
     } else {
-      useVideoRef.current.src = 'https://media.w3.org/2010/05/bunny/movie.mp4'
-      useVideoRef2.current.src = 'https://media.w3.org/2010/05/bunny/movie.mp4'
+      useVideoRef.current.src = video1
+      useVideoRef2.current.src = video1
+      setFlag(true)
     }
     useVideoRef.current.play()
     useVideoRef2.current.play()
-  }, [])
+  }, [flag])
   return (
     <div>
       <header>
