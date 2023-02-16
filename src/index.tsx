61fe8557ddfcf681bb8a959814743d8c678244f1
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-02-16 15:26:50 +0100

    feat: implement mui and styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { createTheme } from '@mui/material'
 
 import App from './App'
 import './style.css'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#fff' },
+  },
+})
+
 render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
