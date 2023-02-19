0b0c4e010d0e3e51951eb571376f2400bbdefb26
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-02-19 18:05:41 +0100

    refactor: small cleanup

--- src/Pages/CreatePage/CreatePage.tsx
+++ src/Pages/CreatePage/CreatePage.tsx
@@ -6,18 +6,12 @@ import { useAdminStore } from '../../stores'
 
 export const CreatePageRaw = () => {
   const setProgram = useAdminStore((s) => s.setProgram)
-  const setCanEditTreeMap = useAdminStore((s) => s.setCanEditTreeMap)
-  const canEditTreeMap = useAdminStore((s) => s.canEditTreeMap)
-
   useEffect(() => {
     setProgram(program)
   }, [setProgram])
 
   return (
     <>
-      <button onClick={() => setCanEditTreeMap(!canEditTreeMap)}>
-        toggle edit
-      </button>
       <TreeList />
     </>
   )
