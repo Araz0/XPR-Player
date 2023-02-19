9fd6ededf42e8581b6a2c4bd4dc9110f20431cb5
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-02-19 22:42:12 +0100

    WIP: create program when not loaded

--- src/components/TreeList/TreeList.tsx
+++ src/components/TreeList/TreeList.tsx
@@ -1,10 +1,13 @@
-import { memo, useEffect, useState } from 'react'
+import { memo, useCallback, useEffect, useRef, useState } from 'react'
 
 import styled from 'styled-components'
 
+import { NoteAdd } from '@mui/icons-material'
+import { Typography, Divider, TextField, IconButton } from '@mui/material'
+
 import { useAdminStore } from '../../stores'
 import { SegmentType } from '../../types'
-import { getIntroSegment } from '../../utils'
+import { generateNewId, getIntroSegment } from '../../utils'
 import { TreeItem } from '../TreeItem'
 
 import './style.css'
@@ -12,13 +15,29 @@ import './style.css'
 const StyledUl = styled.ul`
   padding-bottom: 50px;
 `
+const StyledActionsContainer = styled.div`
+  max-width: 720px;
+  margin-inline: auto;
+  margin-top: 100px;
+`
 
 export const TreeListRaw = () => {
+  const titleRef = useRef<HTMLInputElement>()
   const program = useAdminStore((s) => s.program)
+  const setProgram = useAdminStore((s) => s.setProgram)
+
   console.log('🚀 ~ file: TreeList.tsx:18 ~ TreeListRaw ~ program', program)
   const [introSegment, setIntroSegment] = useState<SegmentType | undefined>(
     undefined
   )
+
+  const handleCreateProgram = useCallback(() => {
+    setProgram({
+      id: generateNewId(),
+      title: titleRef.current?.value || 'New Program',
+      segments: [],
+    })
+  }, [setProgram])
 
   useEffect(() => {
     if (!program) return
@@ -27,6 +46,25 @@ export const TreeListRaw = () => {
     setIntroSegment(intro)
   }, [program])
 
+  if (!program)
+    return (
+      <StyledActionsContainer>
+        <Typography variant="h6">Create new Program</Typography>
+        <Divider />
+        <Typography variant="subtitle1">
+          please fill the input field and submit to continue creating the
+          program.
+        </Typography>
+        <TextField
+          inputRef={titleRef}
+          placeholder={'program title'}
+          size="small"
+        />
+        <IconButton onClick={handleCreateProgram}>
+          <NoteAdd />
+        </IconButton>
+      </StyledActionsContainer>
+    )
   if (!introSegment) return null
   return (
     <>
