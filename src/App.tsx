9f3023665973a3d89c31401f41d1966dfa05d659
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-02-25 21:45:55 +0100

    refactor: routs and home page cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ import { AdminPage, ProgramsPage, Home, ScreenPage, CreatePage } from './Pages'
 function App() {
   return (
     <Routes>
-      <Route index element={<ScreenPage />} />
-      <Route path="home" element={<Home />} />
-      <Route path="app/:screenId/*" element={<ScreenPage />} />
+      <Route index element={<Home />} />
+      <Route path="screen/:screenId/*" element={<ScreenPage />} />
       <Route path="admin" element={<AdminPage />} />
       <Route path="admin/create" element={<CreatePage />} />
       <Route path="admin/programs" element={<ProgramsPage />} />
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,43 +1,7 @@
-import React, { memo, useCallback, useRef } from 'react'
-
-import { useParams } from 'react-router-dom'
-
-import { VideoPlayer } from '../../components/VideoPlayer'
-import { useVideoService } from '../../services'
+import { memo } from 'react'
 
 export const HomeRaw = () => {
-  const { init, playVideo, pauseVideo, setSource, toggleMute, getDuration } =
-    useVideoService()
-  const { screenId } = useParams()
-  const useVideoRef = useRef<any>()
-
-  const handleClick = useCallback(() => {
-    init(useVideoRef)
-    setSource('https://media.w3.org/2010/05/bunny/movie.mp4')
-  }, [init, setSource])
-
-  const handlePlayPause = useCallback(() => {
-    if (useVideoRef.current.paused) {
-      playVideo()
-    } else {
-      pauseVideo()
-    }
-  }, [pauseVideo, playVideo])
-
-  return (
-    <div>
-      <header>
-        <p>
-          Edit <code>src/App.tsx/{screenId}</code> and save to reload.
-        </p>
-        <button onClick={handleClick}>init</button>
-        <button onClick={handlePlayPause}>PlayPause</button>
-        <button onClick={toggleMute}>toggleMute</button>
-        <button onClick={getDuration}>getDuration</button>
-        <VideoPlayer ref={useVideoRef} />
-      </header>
-    </div>
-  )
+  return <div>Home</div>
 }
 
 export const Home = memo(HomeRaw)
