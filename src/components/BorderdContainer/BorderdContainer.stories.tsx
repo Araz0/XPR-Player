6719f4101810ae3324c5fc913612657212974159
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-03-23 18:14:47 +0100

    feat: created styled advanced borderd container

---
+++ src/components/BorderdContainer/BorderdContainer.stories.tsx
@@ -0,0 +1,24 @@
+import { useRef } from 'react'
+
+import { Story } from '@storybook/react'
+
+import { BorderdContainer } from './BorderdContainer'
+
+export const Default: Story = (args) => {
+  const btnsRef = useRef<any>(null)
+
+  return (
+    <BorderdContainer hotRef={btnsRef}>
+      <div>
+        <button>button 1</button>
+      </div>
+      <button ref={btnsRef}>This gives a special hover</button>
+    </BorderdContainer>
+  )
+}
+Default.args = {}
+Default.storyName = 'BorderdContainer'
+
+export default {
+  title: 'BorderdContainers',
+}
---
+++ src/components/BorderdContainer/BorderdContainer.tsx
@@ -0,0 +1,66 @@
+import { memo, useCallback, useEffect, useState } from 'react'
+
+import styled from 'styled-components'
+
+import {
+  PRIMARY_COLOR,
+  BACKGROUND_COLOR,
+  PRIMARY_GRADIENT,
+} from 'constants/styles'
+
+const StyledContainerBorder = styled.div<{ hotBorder: boolean }>`
+  border-radius: 10px;
+  position: relative;
+  background: ${BACKGROUND_COLOR};
+  padding: 2px;
+
+  :hover {
+    cursor: pointer;
+    ${(props) => !props.hotBorder && `background: ${PRIMARY_COLOR};`}
+    ${(props) =>
+      props.hotBorder &&
+      `background: linear-gradient(160deg, ${PRIMARY_GRADIENT});`}
+  }
+`
+const StyledContainer = styled.div`
+  background-color: ${BACKGROUND_COLOR};
+  border-radius: 8px;
+  padding: 1rem;
+`
+
+export type BorderdContainerProps = {
+  children: React.ReactNode
+  hotRef?: React.RefObject<HTMLDivElement>
+}
+export const BorderdContainerRaw = ({
+  children,
+  hotRef,
+}: BorderdContainerProps) => {
+  const [hotHoverd, setHotHoverd] = useState<boolean>(false)
+
+  const handleOnHover = useCallback(() => {
+    setHotHoverd(true)
+  }, [])
+  const handleOnNotHoverd = useCallback(() => {
+    setHotHoverd(false)
+  }, [])
+
+  useEffect(() => {
+    if (!hotRef) return
+    hotRef?.current?.addEventListener('mouseenter', handleOnHover)
+    hotRef?.current?.addEventListener('mouseleave', handleOnNotHoverd)
+    return () => {
+      hotRef?.current?.removeEventListener('mouseenter', handleOnHover)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      hotRef?.current?.removeEventListener('mouseleave', handleOnNotHoverd)
+    }
+  }, [handleOnHover, handleOnNotHoverd, hotRef])
+
+  return (
+    <StyledContainerBorder hotBorder={hotHoverd}>
+      <StyledContainer>{children}</StyledContainer>
+    </StyledContainerBorder>
+  )
+}
+
+export const BorderdContainer = memo(BorderdContainerRaw)
