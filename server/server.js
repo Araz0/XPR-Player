0b0c57bbd70ef9279b42dda0b61e9724ee22fe81
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2023-04-24 02:03:18 +0200

    WIP: implement a http api that emits event on get requests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const http = require('http')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const { networkInterfaces } = require('os')
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require('express')
@@ -18,9 +20,38 @@ const io = new Server(server, {
 let clients = []
 let program = undefined
 
+// // get the ip of the express app - start
+// const nets = networkInterfaces()
+// const results = Object.create(null) // Or just '{}', an empty object
+
+// for (const name of Object.keys(nets)) {
+//   for (const net of nets[name]) {
+//     // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+//     // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+//     const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+//     if (net.family === familyV4Value && !net.internal) {
+//       if (!results[name]) {
+//         results[name] = []
+//       }
+//       results[name].push(net.address)
+//     }
+//   }
+// }
+// // eslint-disable-next-line no-console
+// console.log(results)
+// // get the ip of the express app - end
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello world</h1>')
   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (req, res) => {
+  const selectedIndex = req.query.selectedIndex
+  io.emit('user-selected-segment', selectedIndex)
+  console.log('🚀 - user-selected-segment', selectedIndex)
+  // Send a response to the HTTP request
+  res.send('Socket event emitted successfully')
 })
 
 server.listen(port, () => {
